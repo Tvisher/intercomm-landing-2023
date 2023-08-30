5ddb42b7f82535e708c955a869303fdbffc98982
Ivan Tvisher <Tvisher@mail.ru>
2023-08-30 15:31:38 +0600

    add some animation

--- src/js/app.js
+++ src/js/app.js
@@ -26,7 +26,7 @@ window.addEventListener('load', (e) => {
     setTimeout(() => {
         AOS.init({
             delay: 0, // values from 0 to 3000, with step 50ms
-            duration: 800, // values from 0 to 3000, with step 50ms
+            duration: 1200, // values from 0 to 3000, with step 50ms
             easing: 'ease', // default easing for AOS animations
             once: true, // whether animation should happen only once - while scrolling down
         });
